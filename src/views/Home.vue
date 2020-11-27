5a4f975f8b38fee07c66b09a623bf456f7cdf734
xuanqiyan <17310920903@163.com>
2020-11-27 10:59:35 +0800

    基于vue的滑块验证

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <button @click='verify' >验证滑块</button>
+        <Vcode 
+            :show="isShow"
+            @fail='onfail' 
+            successText='验证成功' 
+            failText='验证失败'
+            :imgs='[Img1,Img2]'
+            @success="onSuccess" />
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Vcode from "vue-puzzle-vcode";
+
+import Img1 from '@/assets/3.jpeg';
+import Img2 from '@/assets/5.jpeg';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    data() {
+        return {
+            isShow: false, // 验证码模态框是否出现,
+            Img1,
+            Img2,
+        };
+    },
+    components: {
+        Vcode,
+    },
+    methods: {
+        verify(){
+            this.isShow = true
+        },    
+        // 用户通过了验证
+        onSuccess(msg) {
+            console.log(msg)
+            this.isShow = false; // 通过验证后，需要手动隐藏模态框
+        },
+        // 用户未通过验证
+        onfail(){
+            console.log('fail')
+        }
+    },
+};
 </script>
